f45f1f8225c3a96bcddc70fd04b39d8e61a9bddd
Stephen Prasetya Chrismawan <stephenprasetyachrismawan27@gmail.com>
2023-06-15 20:52:04 +0700

    commit

---
+++ database/migrations/2023_06_15_133807_create_task_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('task', function (Blueprint $table) {
+            $table->id();
+            $table->string('judul');
+            $table->string('deskripsi');
+            $table->foreignIdFor(User::class);
+            $table->enum('status', ['completed', 'incomplete']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('task');
+    }
+};
